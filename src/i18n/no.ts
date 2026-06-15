2fac1a77c0c0399a3a817f44b8e318acf950648c
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2026-06-15 23:21:10 +0200

    Grupper admin-lenker i en submeny i navigasjonsmenyen (#116)

--- src/i18n/no.ts
+++ src/i18n/no.ts
@@ -34,6 +34,7 @@ const no = {
         tilbakemeldinger: 'Tilbakemeldinger',
         sprak: 'Språk',
         jokerbruk: 'Jokerbruk',
+        admin: 'Admin',
     },
     grupper: {
         tittel: 'Grupper',
